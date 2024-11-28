702a69d4458daa85850d09f765f4e626f2e6107b
Jelius <jelius.basumatary.sama@gmail.com>
2024-11-28 15:52:35 +0000

    Initial commit

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -10,33 +10,33 @@ export default function Footer() {
         <footer>
             <p className="font-bold text-lg mt-10 mb-2">Steps to re-create the bug:</p>
             {pathname !== "/" ? (
-                <li>Navigate to <span className="text-red-400">"home page"</span> by clicking the link above.</li>
+                <li>Navigate to <span className="text-red-400">&quot;home page&quot;</span> by clicking the link above.</li>
             ) : (
                 <>
                     <li>Refresh the page just to make sure nothing is cached.</li>
-                    <li>Navigate to <span className="text-red-400">"art page"</span> by clicking the link above.</li>
+                    <li>Navigate to <span className="text-red-400">&quot;art page&quot;</span> by clicking the link above.</li>
                 </>
             )}
 
             <span className="mt-2 flex flex-nowrap flex-col gap-y-2">
                 <span className="font-bold">
-                    When navigating to <span className="text-red-400">"art page"</span> from <span className="text-red-400">"home page"</span> :
+                    When navigating to <span className="text-red-400">&quot;art page&quot;</span> from <span className="text-red-400">&quot;home page&quot;</span> :
                 </span>
 
                 <li>
-                    Notice that the loading UI says <span className="text-red-400">"Loading Artist page..."</span> instead of <span className="text-red-400">"Loading Art page..."</span>.
+                    Notice that the loading UI says <span className="text-red-400">&quot;Loading Artist page...&quot;</span> instead of <span className="text-red-400">&quot;Loading Art page...&quot;</span>.
                 </li>
 
                 <li>
-                    Exactly, if you look into the code, that is the `loading.tsx` UI for the <span className="text-red-400">"artist page"</span> and not the <span className="text-red-400">"art page"</span>.
+                    Exactly, if you look into the code, that is the `loading.tsx` UI for the <span className="text-red-400">&quot;artist page&quot;</span> and not the <span className="text-red-400">&quot;art page&quot;</span>.
                 </li>
 
                 <li>
-                    But for some unknown reasons the `loading.tsx` UI for <span className="text-red-400">"art page"</span> is not displayed.
+                    But for some unknown reasons the `loading.tsx` UI for <span className="text-red-400">&quot;art page&quot;</span> is not displayed.
                 </li>
                 
                 <li>
-                    Instead `loading.tsx` UI for <span className="text-red-400">"artist page"</span> is displayed when navigating to <span className="text-red-400">"art page"</span>.
+                    Instead `loading.tsx` UI for <span className="text-red-400">&quot;artist page&quot;</span> is displayed when navigating to <span className="text-red-400">&quot;art page&quot;</span>.
                 </li>
             </span>
         </footer>
